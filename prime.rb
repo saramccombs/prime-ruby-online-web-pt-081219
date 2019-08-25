2dcdbef928e5b5d0ab46be007575e8bac8b104e4
Sara McCombs <thesaramccombs@gmail.com>
2019-08-25 16:03:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
-# method named prime 
+# method named prime? 
 # method returns true for prime numbers
 # method returns false for non-prime numbers
 # 
 # What is a prime number? "A prime number is a whole number greater than 1 whose only factors are 1 and itself. A factor is a whole numbers that can be divided evenly into another number."
 
-def prime(number)
+def prime?(number)
+  if number < 0 || number ==
+  else
+  end
   
 end
