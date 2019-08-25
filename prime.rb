b5f199582e2a42f0c5ef171c75d5825bc1d73d7b
Sara McCombs <thesaramccombs@gmail.com>
2019-08-25 15:46:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 # method returns true for prime numbers
 # method returns false for non-prime numbers
 # 
-# What is a prime number? 
+# What is a prime number? "A prime number is a whole number greater than 1 whose only factors are 1 and itself. A factor is a whole numbers that can be divided evenly into another number."
 
 def prime(number)
   
